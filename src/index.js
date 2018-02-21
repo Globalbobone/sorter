f5c09a7d7fdb854881160ff2aaf8917f787a9171
Aliaksei Pliakhanau <3253255@rambler.ru>
2018-02-21 14:25:13 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
 class Sorter {
   constructor() {
-    // your implementation
+    //this.count = 0;
+    this.array = [];
+    this.res=[];
+    this.comp = null;
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
+    //this.count++;
   }
 
   at(index) {
-    // your implementation
+    return this.array[index];
+    //this.count--;
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
   sort(indices) {
-    // your implementation
+    var ind = indices; 
+    ind = ind.sort(function compareNumbers(a, b) {
+      return a - b;
+    });
+    
+    for(var i = 0; i < ind.length; i++) {
+      this.res.push(this.array[ind[i]]);
+    }
+    
+    if (this.comp == null) {
+      this.res = this.res.sort(function(a, b) {
+        return a - b;   
+      });
+    }
+    else {
+      this.res = this.res.sort(this.comp);
+    }
+   
+    for(var i = 0; i < this.res.length; i++ ) {
+      this.array[ind[i]] = this.res[i];
+    }
+    
+    this.res.length = 0;
+    return this.array;
+    
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comp = compareFunction;
   }
 }
 
